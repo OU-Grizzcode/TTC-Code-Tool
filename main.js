dde4ef1485c84c5aef57768ae9fedd0e5a7087ae
klayton smith <motoklay@gmail.com>
2020-06-29 15:08:29 -0400

    pause

--- main.js
+++ main.js
@@ -9,18 +9,19 @@ var interpretationStrat = new CInterpreter();;
 var challenges;
 $.getJSON("./Challenges.json", (json) => {
 	challenges = json;
-	$("#challenge-name").text("Challenge 1: " + challenges[0].name);
+	$("#challenge-name").text(challenges[0].name);
 	$("#challenge-description").text(challenges[0].description);
 });
 
 function run() {
 
+	var testCases = challenges[0].testCases;
+
 	var code = editor.getValue();
-	var cases = challenges[0].testCases;
-	for (let i in cases) {
-		var input = cases[i].input;
+	for (let i in testCases) {
+		var input = testCases[i].input;
 		const response = interpretationStrat.run(code, input);
-		if (response == cases[i].output) alert("success");
+		if (response == testCases[i].output) alert("success");
 		else alert("fail");
 	}
 }
